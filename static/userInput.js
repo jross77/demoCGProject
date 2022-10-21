0300b635e3099767d39974dfa0ba321a6f50b036
Julia Ross <junioross12@gmail.com>
2022-10-21 13:14:59 -0400

    pause for the day

--- static/userInput.js
+++ static/userInput.js
@@ -1,12 +1,9 @@
-//Do we want the user to upload a file is the string is very long?
-    // Get the button and container elements from HTML:
-    const button = document.getElementById("submit_dna");
-    const data_s = document.getElementById("dna_seq");
-    const show_img = document.getElementById("info");
-    
-    button.onclick = sendFunc();
-    // Create an event listener on the button element:
+
+
     function sendFunc(){
+        const button = document.getElementById("submit_dna");
+        const data_s = document.getElementById("dna_seq");
+        const show_img = document.getElementById("info");
         // Get the reciever endpoint from Python using fetch:
         // GET is the default method, so we don't need to set it
         // Print entered dna string for testing purposes
@@ -17,12 +14,5 @@
         type:"POST",
         contentType: "application/json",
         data: JSON.stringify(s)});
-
-    output = $.ajax({
-        url:"/test",
-        type:"GET",
-        contentType: "application/json",
-        data: JSON.stringify(s)});
-    console.log(output);
     } 
    
